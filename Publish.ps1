8273fd229ab861309774b5c6d1f328c95c359a71
linka <lqadm@qq.com>
2022-07-04 23:27:10 +0800

    chore: update script

--- Publish.ps1
+++ Publish.ps1
@@ -13,7 +13,8 @@ Param(
   [switch]$Minor,
   [switch]$Major,
   [switch]$Commit,
-  [switch]$Tag
+  [switch]$Tag,
+  [switch]$Beta
 )
 
 $dir = ".\src"
@@ -22,8 +23,18 @@ $currentVersion = (Test-ModuleManifest "$dir\Register-Completion.psd1").version
 @{
   "PowerShell Version" = $PSVersionTable.PSVersion;
   "Module Version" = $currentVersion
-} | % { new-object PSObject -Property $_} | Format-List
+} | ForEach-Object { new-object PSObject -Property $_} | Format-List
 
+function Get-BetaVersion {
+  $prerelease = (Test-ModuleManifest "$dir\Register-Completion.psd1").PrivateData.PSData.Prerelease
+  if ($prerelease) {
+    $betaVersion = $prerelease.Substring(4)
+  }
+  else {
+    $betaVersion = 0
+  }
+  return [Int32]$betaVersion
+}
 function Set-SemverVersion {
   param(
     [PSCustomObject]$version,
@@ -55,26 +66,51 @@ if ($Action -eq 'help') {
   @{
     "1" = ".\Publish.ps1 release -Patch -Commit -Tag"
     "2" = ".\Publish.ps1 reset -Tag"
-  } | % { new-object PSObject -Property $_} | Format-List
+  } | ForEach-Object { new-object PSObject -Property $_} | Format-List
 }
 elseif ($Action -eq 'release') {
-  if ($Patch) { $newVersion = Set-SemverVersion $currentVersion -patch }
-  elseif ($Minor) { $newVersion = Set-SemverVersion $currentVersion -minor }
-  elseif ($Major) { $newVersion = Set-SemverVersion $currentVersion -major }
-  else { $newVersion = Read-Host "Input a new version(v$currentVersion)" }
 
-  $confirm = Read-Host "Confirm to release v$($newVersion)(previous v$($currentVersion))?(y/n)" [Char]
+  $betaVersionNum = Get-BetaVersion
+
+  if (($Beta -And ($betaVersionNum -eq 0)) -Or !$Beta) {
+    if ($Patch) { $newVersion = Set-SemverVersion $currentVersion -patch }
+    elseif ($Minor) { $newVersion = Set-SemverVersion $currentVersion -minor }
+    elseif ($Major) { $newVersion = Set-SemverVersion $currentVersion -major }
+    else { $newVersion = Read-Host "Input a new version(v$currentVersion)" }
+  }
+  else {
+    $newVersion = $currentVersion
+  }
+
+  if ($Beta) {
+    $betaVersion = "beta$($betaVersionNum + 1)"
+    $fullVersion =  "$newVersion-$betaVersion"
+  }
+  else {
+    $fullVersion = $newVersion
+  }
+  Write-Host "You are about to release a version($fullVersion)`n" -ForegroundColor Yellow
+
+  $confirm = Read-Host "Confirm to release v$($fullVersion)(previous v$($currentVersion))?(y/n)" [Char]
 
   if ($confirm -eq 'y') {
-    Update-ModuleManifest -Path "$dir\Register-Completion.psd1" -ModuleVersion $newVersion
+    if ($Beta) {
+      Update-ModuleManifest -Path "$dir\Register-Completion.psd1" -ModuleVersion $newVersion -Prerelease $betaVersion
+    }
+    else {
+      $d = "$dir\Register-Completion.psd1"
+      (Get-Content $d -Raw) -Replace "Prerelease = 'beta\d+'", "# Prerelease = ''" | Set-Content $d
+      Update-ModuleManifest -Path "$dir\Register-Completion.psd1" -ModuleVersion $newVersion
+    }
+
     npx standard-changelog
 
     if ($Commit) {
       git add "$dir\Register-Completion.psd1" CHANGELOG.md
-      git commit -m "Release v$newVersion"
+      git commit -m "Release v$fullVersion"
     }
     if ($Tag) {
-      git tag --annotate --message "v$newVersion" v$newVersion
+      git tag --annotate --message "v$fullVersion" v$fullVersion
     }
     $comfirmPush = Read-Host "Confirm to push?(y/n)" [Char]
     if ($comfirmPush -eq 'y') {
@@ -89,17 +125,25 @@ elseif ($Action -eq 'release') {
 }
 elseif ($Action -eq 'reset') {
   $version = (Test-ModuleManifest "$dir\Register-Completion.psd1").version
-  $comfirmPush = Read-Host "Are you sure reset the version($version)?(y/n)" [Char]
+  $betaVersionNum = Get-BetaVersion
+  if ($Beta) {
+    $betaVersion = "beta$($betaVersionNum + 1)"
+    $fullVersion =  "$version-$betaVersion"
+  }
+  else {
+    $fullVersion = $version
+  }
+  $comfirmPush = Read-Host "Are you sure reset the version($fullVersion)?(y/n)" [Char]
   if ($comfirmPush -eq 'y') {
     git reset --hard HEAD~1
     if ($Tag) {
-      git tag -d "v$version"
+      git tag -d "v$fullVersion"
     }
-    $comfirmPushRemote = Read-Host "Are you sure reset the version($version) to remote?(y/n)" [Char]
+    $comfirmPushRemote = Read-Host "Are you sure reset the version($fullVersion) to remote?(y/n)" [Char]
     if ($comfirmPushRemote -eq 'y') {
       git push -f
       if ($Tag) {
-        git push origin ":refs/tags/v$version"
+        git push origin ":refs/tags/v$fullVersion"
       }
     }
   }
